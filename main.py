92befe6c8a935a13cc60538e09855911993a033c
Gustavo Faria <gustavosh31@gmail.com>
2020-11-01 15:06:33 -0300

    Creating a method to show game statistics

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
+# Mostra as estátisticas do jogo
+def show_winner_stats(current_player: int = None, player_names: list = None, rounds: int = None, points: list = None):
+    print("----------- Game stats --------------")
+    print(f"{player_names[current_player]} VENCEU!!!!!")
+    print(f"Foram jogados {rounds} rounds para acabar a partida")
+    print(f"{player_names[0]} {points[0]} X {player_names[1]} {points[1]}")
+
+# Converte se a pessoa deseja continuar o jogo
+def convert_continue(text: str = None):
+    text.upper()
+
+    if text == "YES":
+        return True
+    else:
+        return False
+
+# Converte a linha para o número correto
+def convert_line(line: str = None):
+    line = int(line)
+    line -= 1
+    return str(line)
+
 # Converte a coluna para número
 def convert_column(column: str = None):
     if column == 'A':
-        return 0
+        return '0'
     elif column == 'B':
-        return 1
+        return '1'
     elif column == 'C':
-        return 2
+        return '2'
     else:
         return None
 
 # Valida a posição desejada pelo jogador
 def validate_position(table: list = None, position: str = None):
     valid = True
+
     if position is None:
         valid = False
-    else:
-        position[1] = convert_column(position[1])
 
     if position[1] is None:
+        valid = False
+
+    if table[int(position[0])][int(position[1])] != '|  -  |':
         valid = False
 
     return valid
@@ -29,7 +53,6 @@ def save_player_name():
 
     return [player1_name, player2_name]
 
-
 # Verifica o atual jogador
 def check_current_player(current_round: int = None, current_player: int = None):
 
@@ -38,37 +61,36 @@ def check_current_player(current_round: int = None, current_player: int = None):
             current_player += 1
         else:
             current_player -= 1
+
     return current_player
 
 # Verifica se alguém ganhou o jogo
 def check_player_play(table: list = None):
 
     somebody_won = False
-
     # Verifica todas as linhas
-    if table[1][1] == table[1][2] == table[1][3]:
+    if table[0][0] == table[0][1] == table[0][2]:
         somebody_won = True
-    elif table[2][1] == table[2][2] == table[2][3]:
+    elif table[1][0] == table[1][1] == table[1][2]:
         somebody_won = True
-    elif table[3][1] == table[3][2] == table[3][3]:
+    elif table[2][0] == table[2][1] == table[2][2]:
         somebody_won = True
 
     # Verifica todas as colunas
-    elif table[1][1] == table[2][1] == table[3][1]:
+    elif table[0][0] == table[1][0] == table[2][0]:
         somebody_won = True
-    elif table[1][2] == table[2][2] == table[3][2]:
+    elif table[0][1] == table[1][1] == table[2][1]:
         somebody_won = True
-    elif table[1][3] == table[2][3] == table[3][3]:
+    elif table[0][2] == table[1][2] == table[2][2]:
         somebody_won = True
 
     # Verifica as diagonais
-    elif table[1][1] == table[2][2] == table[3][3]:
+    elif table[0][0] == table[1][1] == table[2][2]:
         somebody_won = True
-    elif table[1][3] == table[2][2] == table[3][1]:
+    elif table[2][0] == table[1][1] == table[0][3]:
         somebody_won = True
 
     return somebody_won
-
 
 # Imprime o tabuleiro
 def print_board(table: list = None):
@@ -96,15 +118,32 @@ while continue_game:
 
     current_round = 1
     current_player = 0
+
     while not game_finished:
         print("\n")
         print_board(table)
         current_player = check_current_player(current_round, current_player)
 
-        position = input("Digite a linha e coluna que deseja preencher: ")
+        position = input("Digite a linha e coluna que deseja preencher: ").upper()
+        position = position.replace(position[0], convert_line(position[0]))
+        position = position.replace(position[1], convert_column(position[1]))
+        valid_position = validate_position(table, position)
 
+        while not valid_position:
+            print("Jogada inválida, por favor escolha novamente")
+            position = input("Digite a linha e coluna que deseja preencher: ")
+            valid_position = validate_position(table, position)
 
+        if current_player == 0:
+            table[int(position[0])][int(position[1])] = '|  X  |'
+        else:
+            table[int(position[0])][int(position[1])] = '|  O  |'
 
+        if current_round >= 5:
+            game_finished = check_player_play(table)
+        current_round += 1
 
-        break
-    break
+    print_board(table)
+    show_winner_stats(current_player, players_name, current_round, players_points)
+    continue_game_string = input("Continuar jogo? YES/NO: ")
+    continue_game = convert_continue(continue_game_string)
